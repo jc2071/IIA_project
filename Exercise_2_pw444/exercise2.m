7c6739b14e2aa93eec96dbb849c60ff5a6cb6fb4
petroselo <paultheskierguy@gmail.com>
2020-05-07 23:47:55 +0100

    Script for exercise 2

---
+++ Exercise_2_pw444/exercise2.m
@@ -0,0 +1,47 @@
+%Script file for Exercise 2
+
+%Reset Workspace
+clear
+close all
+
+%Set parameters
+xmin = -2.5;
+xmax = 2.5;
+nx = 51;
+
+ymin = -2;
+ymax = 2;
+ny = 41;
+
+del = 1.5;
+
+%Create matrices
+x = linspace(xmin, xmax, nx);
+y = linspace(ymin, ymax, ny);
+[xm, ym] = meshgrid(x, y);
+
+[infa, infb] = refpaninf(del, xm, ym);
+
+c = -0.15:0.05:0.15;
+contour(xm, ym, infa, c)
+figure(2)
+contour(xm, ym, infb, c)
+
+%For discrete approximation set up approximate result matrices
+nv = 100;
+aprxinfa = zeros(size(xm));
+aprxinfb = zeros(size(xm));
+
+%Loop over each discrete vortex and seperate out the gamma_a and gamma_b
+%coefficients
+for k = 1:nv
+    xc = (k-0.5)*del/nv;
+    yc = 0;
+    aprxinfa = aprxinfa + psipv(xc, yc, (1-(k-0.5)/nv)*del/nv, xm , ym);
+    aprxinfb = aprxinfb + psipv(xc, yc, ((k-0.5)/nv)*del/nv, xm , ym);
+end
+
+figure(3)
+contour(xm, ym, aprxinfa, c)
+figure(4)
+contour(xm, ym, aprxinfb, c)
