2c5074dd1d283346b630f8e25ac2575d1016bd96
petroselo <paultheskierguy@gmail.com>
2020-05-07 20:55:25 +0100

    Update function descriptor

--- psipv.m
+++ psipv.m
@@ -1,4 +1,5 @@
-%
 function psixy = psipv(xc, yc, Gamma, x, y)
+%Return the value of the streamfunction at a certain point due to a point
+%vortex
     psixy = -Gamma*log((x-xc)^2+(y-yc)^2)/(4*pi);
 end
