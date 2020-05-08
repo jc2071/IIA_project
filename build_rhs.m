bf452341c7bde7c0394ddf343c75e7aef823300a
petroselo <paultheskierguy@gmail.com>
2020-05-08 18:53:21 +0100

    Fix build rhs

--- build_rhs.m
+++ build_rhs.m
@@ -1,5 +1,6 @@
 function rhsvec = build_rhs(xs, ys, alpha)
-%Return b vector of psifs(i) - psifs(i+1)
-    psifs = ys*cos(alpha) - xs*sin(alpha);
-    rhsvec = psifs - circshift(psifs, -1);
+%Return b vector of psi_fs(i) - psifs(i+1) with 0s top and bottom
+    psi_fs = (ys*cos(alpha) - xs*sin(alpha));
+    temp = ( circshift(psi_fs, 1) - psi_fs );    
+    rhsvec = [0, temp(2:length(temp)-1), 0]'; %t ransposed to col
 end
