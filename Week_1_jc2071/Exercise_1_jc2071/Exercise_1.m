1c3704b3e4b99e0a1a413ca595c6ae95f0d31798
Jonathan Collins <jc2071@cam.ac.uk>
2020-05-09 22:13:37 +0100

    Changed saving location

--- Week_1_jc2071/Exercise_1_jc2071/Exercise_1.m
+++ Week_1_jc2071/Exercise_1_jc2071/Exercise_1.m
@@ -10,12 +10,14 @@ x = linspace(-2.5,2.5,N);
 y = linspace(-2,2,N);
 [xg,yg] = meshgrid(x,y);
 
+
 c = -0.4:0.2:1.2;
 [C,h] = contour(xg,yg,psipv(xc,yc,gamma,xg,yg),c);
 title("Point Vortex Stream Function " + '$\psi = \frac{\Gamma}{4\pi}log(r^2)$','Interpreter','latex');
 xlabel("x");
 ylabel("y");
-text(-2.3,-1.8,'Vortex at x = 0.5, y = 0.25','FontName','Latin Modern Math')
+text(-2.3,-1.8,'Vortex at x = 0.5, y = 0.25','FontName','Times')
 clabel(C,h,c,'FontName','Times')
-set(gca,'fontname','Times', 'FontSize',12, 'TickLabelInterpreter','latex');
-print('-painters','-depsc','Exercise_1_coutour_plot.eps')
+set(gca,'fontname','Times', 'FontSize',12, 'TickLabelInterpreter','latex','Units','inches');
+box on
+print (gcf, '/Users/MCJON/Documents/Part IIA SA1 Project/IIA_project/LaTeX/Week_1\Exercise_1_Contourplot', '-deps2' )
