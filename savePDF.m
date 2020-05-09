c80a53c441fe5f675006c2018c2432b42602d05b
Jonathan Collins <jc2071@cam.ac.uk>
2020-05-09 22:17:10 +0100

    removed save PDF

--- savePDF.m
+++
@@ -1,7 +0,0 @@
-function savePDF(plot_name)
-fig = gcf;
-fig.PaperPositionMode = 'auto';
-fig_pos = fig.PaperPosition;
-fig.PaperSize = [fig_pos(3) fig_pos(4)];
-print(fig,'-dpdf','-painters','-r600','-bestfit');
-end
