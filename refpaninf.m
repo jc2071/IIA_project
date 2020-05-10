33ecc161ff1792b47c6f3cd774b6d160cb1028ae
petroselo <paultheskierguy@gmail.com>
2020-05-10 15:09:42 +0100

    Update refpaninf to use more elementwise operations so it can take X and Y as 3d arrays and del as a k-vector

--- refpaninf.m
+++ refpaninf.m
@@ -1,25 +1,29 @@
 function [infa, infb] = refpaninf(del, X, Yin)
-%Calculate the influence coefficients for a reference panel of vorticity
+% Calculate the influence coefficients for a reference panel of vorticity.
+% X and Y can be 3D arrays with del a k-index vector giving length of each
+% panel.
 
+%This still needs sorting out
 Y = zeros(size(Yin));
-for k = 1:numel(Yin)
-    if abs(Yin(k)) < 1e-8
-        Y(k) = 1e-8; % why not using a *sign(Yin)??? why does it nan my matrix
+for n = 1:numel(Yin)
+    if abs(Yin(n)) < 1e-8
+        Y(n) = 1e-8; % why not using a *sign(Yin)??? why does it nan my matrix
     else
-        Y(k) = Yin(k);
+        Y(n) = Yin(n);
     end
 end
 
 
 %Calculate integrals using elemenet operations where neccesary
-I0 = -( X.*log(X.^2 + Y.^2) - (X-del).*log((X-del).^2 + Y.^2)...
+I0 = -( X.*log(X.^2 + Y.^2) - (X-del).*log((X-del).^2 + Y.^2) ...
 -2*del +2*Y.*(atan(X./Y)-atan((X-del)./Y)) )/(4*pi);
 
 I1 = ( (X.^2 + Y.^2).*log(X.^2 + Y.^2) ...
--((X-del).^2+Y.^2).*log((X-del).^2+Y.^2) -2*X*del + del^2)/(8*pi);
+-((X-del).^2+Y.^2).*log((X-del).^2+Y.^2) -2*X.*del + del.^2)/(8*pi);
 
-%Calculate influence coefficients
-infa = (1-X/del).*I0 - I1/del;
-infb = (X/del).*I0 + I1/del;
+% Each of these is a 3D array. Elemental division of a 3D array by k-vector
+% divides each sheet by k-vector value at that k index.
+infa = (1-X./del).*I0 - I1./del;
+infb = (X./del).*I0 + I1./del;
 
 end
