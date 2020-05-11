d8796844485c5cef5f42847a11f7947ad037f570
petroselo <paultheskierguy@gmail.com>
2020-05-11 21:53:21 +0100

    blah

--- psipv.m
+++ psipv.m
@@ -5,7 +5,6 @@ function psixy = psipv ( xc, yc, Gamma, x, y )
 % xc, yc are coords of vortex of strength Gamma
 % x, y are matrices at which the streamfunction is evaluated
 % returns a matrix of size(x)
-%
 
 psixy = -Gamma*log((x-xc).^2+(y-yc).^2)/(4*pi);
 
