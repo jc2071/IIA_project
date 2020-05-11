221180c8ba821cfdd9903fe4f6352f956559ea97
Jonathan Collins <jc2071@cam.ac.uk>
2020-05-11 19:21:38 +0100

    updates to e1g1

--- fancyplot.m
+++ fancyplot.m
@@ -5,7 +5,7 @@ title(title1,'Interpreter','latex');
 xlabel(xlabel1);
 ylabel(ylabel1);
 clabel(C,h,c,'FontName','Times', 'FontSize',8)
-set(gca,'fontname','Times', 'FontSize',12);
+set(gca,'fontname','Times', 'FontSize',14);
 axis equal
 
 end
