7a646b718750a62f14bc15576c86cebbf5d23a5e
Jonathan Collins <jc2071@cam.ac.uk>
2020-05-13 20:54:05 +0100

    Doing what is wanted for ex2

--- Week_2_jc2071/Exercise_2/transition.m
+++ Week_2_jc2071/Exercise_2/transition.m
@@ -1,4 +1,4 @@
-function [position, Rethetdiv1000] = transition(Rel, ugrad)
+function [int, ils, Rethetdiv1000] = transition(Rel, ugrad)
 %calculates point of transition for a given velocity gradient and Rel
 
 np = 101; % intergration spacing 
@@ -8,6 +8,9 @@ ustart = 1; % this is Ue/U at x = 0
 ue = linspace(ustart,ustart + ustart*ugrad,np); % this is array of velocity
 
 Thwaites2 = zeros(size(x)); % this is the stepping of the intergral
+int = 0;
+ils = 0;
+Rethetdiv1000 = 0;
 
 i = 1;
 while laminar && i < np
@@ -20,13 +23,26 @@ while laminar && i < np
     Rethet = Rel * ue(i) * sqrt(Thwaites2(i));
     if log(Rethet) >= 18.4*He - 21.74
         laminar = false;
-        position = x(i); 
+        int = i; 
         Rethetdiv1000 = Rethet/1000;
-    else
-        position = "No";
-        Rethetdiv1000 = "No";
+    elseif m >= 0.09
+        ils = i;
+        laminar = false; 
+        Rethetdiv1000 = Rethet/1000;
     end
-end 
 
+if int ~= 0
+    disp(['At Re = ' num2str(Rel) ' with velocity gradient = ' ...
+        num2str(ugrad) ' Natural transition at ' num2str(x(int)),...
+                 ' with Rethet ' num2str(Rethet)])
+elseif ils ~0
+    disp(['At Re = ' num2str(Rel) ' with velocity gradient = ' ...
+        num2str(ugrad) ' Laminar seperation at ' num2str(x(ils)),...
+                 ' with Rethet ' num2str(Rethet)])
+else
+    disp('no seperation')
+    
+end
+end
 
 
---
+++ Week_2_jc2071/Exercise_3/Exercise_3.m
@@ -0,0 +1,2 @@
+
+[int, ils, Rethetdiv1000] = transition(10000, -0.25)
