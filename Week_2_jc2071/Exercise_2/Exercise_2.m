8ab6d4971f2254893675be03893fd4577b0b5be0
Jonathan Collins <jc2071@cam.ac.uk>
2020-05-13 18:12:36 +0100

    updates to ex2

--- Week_2_jc2071/Exercise_2/Exercise_2.m
+++ Week_2_jc2071/Exercise_2/Exercise_2.m
@@ -2,34 +2,15 @@
 clear
 close all
 
-np = 1001; % intergration spacing 
-laminar = true;
-x = linspace(0,1,np); % this is (x/L)
-ustart = 1; % this is Ue/U at x = 0
-ugrad = 0; % this is velocity gradient
-ue = linspace(ustart,ustart + ustart*ugrad,np); % this is array of velocity
-Rel = 20e6;
+ugrads = [-0.1, 0, 0.1];
+Rels = [5e6, 10e6, 20e6];
+results_matrix_location = cell(length(Rels),length(ugrads));
+results_matrix_value = cell(length(Rels),length(ugrads));
 
-Thwaites2 = zeros(size(x)); % this is the stepping of the intergral
-
-i = 1;
-while laminar && i < np
-    i = i +1;
-    Thwaites2(i) = Thwaites2(i-1) +(0.45* ue(end)^-6)/ Rel * ...
-        ueintbit(x(i-1), ue(i-1), x(i), ue(i));
-    m = - Rel* Thwaites2(i) * ugrad;
-    H = thwaites_lookup(m);
-    He = laminar_He(H);
-    Rethet = Rel * ue(i) * sqrt(Thwaites2(i));
-    if log(Rethet) >= 18.4*He - 21.74
-        laminar = false;
-        disp([x(i) Rethet/1000])
+for i = 1:length(ugrads)
+    for j = 1:length(Rels)
+        [location, value] = transition(Rels(j), ugrads(i));
+        results_matrix_location{j,i} = location;
+        results_matrix_value{j,i} = value;     
     end
-end 
-
-
-plot(x(1:i), sqrt(Thwaites2(1:i)));
-hold on
-scatter(x(i),sqrt(Thwaites2(i)),130, 'x');
-
-
+end
