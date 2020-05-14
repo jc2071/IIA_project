e402fa59e40d20c9bfd8c4ccdba958a219302e51
petroselo <paultheskierguy@gmail.com>
2020-05-14 02:23:24 +0100

    Exercise 2

--- Week_2_pw444/exercise1.m
+++ Week_2_pw444/exercise1.m
@@ -3,19 +3,19 @@ close all
 
 % Set parameters
 U = 1;
-Re = 1e3; % Based on reference length L and speed U
+ReL = 1e3; % Based Referencence length L and speed U
 x = linspace(0, 1, 101);
 sz = size(x);
 ue = U*ones(sz);
 int = zeros(sz);
 theta = zeros(sz);
-blasius = 0.664/sqrt(Re)*sqrt(x);
+blasius = 0.664/sqrt(ReL)*sqrt(x);
 
 % We start the loop at the second element as everything is 0 at x = 0
 % We can see about vectorisation later
 for i = 2:length(x)
     int(i) = int(i-1) + ueintbit(x(i-1), ue(i-1), x(i), ue(i));
-    theta(i) = sqrt( 0.45/Re * int(i)/ue(i)^6 );
+    theta(i) = sqrt( 0.45/ReL * int(i)/ue(i)^6 );
 end
 
 plot(x, theta, 'b')
---
+++ Week_2_pw444/exercise2.m
@@ -0,0 +1,42 @@
+clear
+close all
+
+n = 101; % Number of panels
+
+for u_gradient = [-0.1, 0, 0.1]
+    ue = linspace(1, 1+u_gradient, n);
+    disp(['Velocity gradient: ', num2str(u_gradient)])
+    for ReL = [5e6, 1e7, 2e7]
+        disp(['ReL = ', num2str(ReL)])
+        % Reset arrays
+        x = linspace(0, 1, n);
+        int = zeros(n);
+        theta = zeros(n);
+        
+        % Loop parameters
+        laminar = true;
+        i = 1;
+
+        while laminar && i < n
+            i = i + 1; % Effectively start loop at i = 2
+
+            % Calculate momentum thickness
+            int(i) = int(i-1) + ueintbit(x(i-1), ue(i-1), x(i), ue(i));
+            theta(i) = sqrt( 0.45/ReL * int(i)/ue(i)^6 );
+
+            % Check for transtion
+            Rethet = ReL * ue(i) * theta(i);
+            m = -ReL * theta(i)^2 * (ue(i)-ue(i-1)) / (x(i)-x(i-1));
+            He = laminar_He( thwaites_lookup(m) );
+            if log(Rethet) >= 18.4*He - 21.74
+               laminar = false;
+               disp([x(i) Rethet/1000])
+            end
+            if i == n
+               disp('No transition occured') 
+            end
+
+
+        end
+    end
+end
