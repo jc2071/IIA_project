6d0c7bea9867b7c36057e1477b360260b1f63674
petroselo <paultheskierguy@gmail.com>
2020-05-16 18:06:06 +0100

    ueintbit

---
+++ ueintbit.m
@@ -0,0 +1,11 @@
+function f = ueintbit(xa, ua, xb, ub)
+%
+% function f = ueintbit(xa, ua, xb, ub)
+% 
+% Calculate contribution to thwaites integral between a and b
+u_bar = (ua + ub)/2;
+del_u = ub - ua;
+del_x = xb -xa;
+
+f = (u_bar^5 + 5/6*(u_bar^3)*del_u^2 + (u_bar*del_u^4)/16 )*del_x;
+end
