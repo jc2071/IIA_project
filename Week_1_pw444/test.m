a9fb2d033fe17ceba8124625c120bc0a47ed7c60
Jonathan Collins <jc2071@cam.ac.uk>
2020-05-23 02:59:07 +0100

    playing around, not much progress

--- Week_1_pw444/test.m
+++ Week_1_pw444/test.m
@@ -1,8 +1,7 @@
 % Just a file to play around with
 
 %Reset Workspace
-clear
-close all
+clear; close all; clc
 
 %Set parameters
 xmin = -2.5;
@@ -23,7 +22,13 @@ np = 30;
 theta = (0:np)*2*pi/np;
 % xs = r*cos(theta);
 % ys = r*sin(theta)./(xs*4+8) + 0.2*(1-xs.^2); % mess around with y to get a nice shape
-alpha = pi/18;
+
+aoa = 23;
+alpha = -aoa*pi/180;
+
+R = [cos(alpha) -sin(alpha); sin(alpha) cos(alpha)];
+
+
 
 %-- makes JOUKOWSKI AEROFOILS --
 % + imaginary gives camber
@@ -34,13 +39,16 @@ c = 0.1;
 r = a*(1 +c);
 z = -a*c + 1.3i*a*c + r*exp(1i*theta);
 zeta = z + (a^2)./z;
-xs = real(zeta);
-ys = imag(zeta);
+xs_temp = real(zeta);
+ys_temp = imag(zeta);
+m = [xs_temp;ys_temp];
 
-
+points = R*m;
+xs = points(1,:);
+ys = points(2,:);
 %Calculate gamma vector. This assumes that we are in a free stream already
 A = build_lhs(xs,ys);
-b = build_rhs(xs,ys,alpha);
+b = build_rhs(xs,ys,0);
 gam = A\b;
 
 
@@ -49,17 +57,34 @@ gamk = reshape(gam, 1, 1, np+1);
 [infa, infb] = panelinf(xs(1:np), ys(1:np), xs(2:end), ys(2:end), xm, ym);
 
 %free stream + circulation on surface
-psi = ( ym*cos(alpha) - xm*sin(alpha) ) ...
+psi = ( ym*cos(0) - xm*sin(0) ) ...
     + sum(infa.*gamk(1:np) + infb.*gamk(2:np+1), 3);
 
 
+% Now work out cp to get some nice visuals, use meshgrid xm, ym
+
+%cp = 1 - ue^2
+
+cp = zeros(ny-1,nx-1);
+
+for i = 1:ny -1
+    for j = 1:nx -1
+        dpsiy = (psi(i+1,j) - psi(i,j))/ (x(i+1) - x(i));
+        dpsix = (psi(i,j+1) - psi(i,j+1))/ (y(i+1) - y(i));
+        cp(i,j) = 1 - dpsix^2 - dpsiy^2;
+    end
+end
+
 % Plot the streamfunction contours
-%c = -100:0.06:100
-%contour(xm, ym, psi, c)
+c = -10:0.1:10;
+%[C,h] = contourf(xm, ym, psi, c);
+[C,h] = contourf(xm(1:end-1,1:end-1),ym(1:end-1,1:end-1),cp,c);
+set(h,'LineColor','none')
 hold on
 %d = zeros(size(xs));
-%surface([xs;xs],[ys;ys],[d;d], -[gam';gam'].^2+3,'facecol','no','edgecol','interp','linew',2);
-plot(xs,ys, 'color','magenta')
-%hold off
-surf(xm, ym, psi-0.1853)
+%surface([xs;xs],[ys;ys],[d;d], -[gam';gam'].^2+3,'facecol','black','edgecol','interp','linew',2);
+fill(xs,ys,[1 1 1])
+plot(xs,ys, 'color','black', 'linewidth', 1.5)
+hold off
+%surf(xm, ym, psi-0.1853)
 daspect([1 1 1])
