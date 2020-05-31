3a9afda1a95a37e329ab23fb420ac73674eda50a
Jonathan Collins <jc2071@cam.ac.uk>
2020-05-31 14:14:19 +0100

    updates

--- overlay.m
+++ overlay.m
@@ -1,5 +1,3 @@
-close; clear all
-
 number = input('Enter number of overlay: ','s');
 Re = input('Enter Reynolds number: ','s');
 alpha = input('Enter AOA (degree): ','s');
@@ -17,8 +15,8 @@ for i = 1:n
     geometry = input(['Enter Geometry file ' num2str(i) ,'/', num2str(n)  ': ' ],'s');
     secfile = ['Geometry/' geometry '.surf'];
     [xk, yk] = textread( secfile, '%f%f' );
-    parfile = ['Parfiles/' geometry '.txt'];
-    [~ ,np, ~, ~] = par_read(parfile);
+    %parfile = ['Parfiles/' geometry '.txt'];
+    np = 400;
     nphr = 5*np;
     [xshr yshr] = splinefit ( xk, yk, nphr );
     %  Resize section so that it lies between (0,0) and (1,0)
@@ -26,29 +24,29 @@ for i = 1:n
     %  Interpolate to required number of panels (uniform size)
     [xs ys] = make_upanels ( xsin, ysin, np );
     axis([xmin xmax ymin ymax])
-    subplot(3,1,1)
+    figure(10)
     title('Airfoil geometry') 
     hold on
     plot(xs,ys, 'color',colors(i,:),'DisplayName',geometry,'LineWidth', 1.2)
     legend
     hold off
-    subplot(3,1,2)
-    title('Pressure distrubution') 
-    hold on
-    load(['Data/' geometry '/' Re '_' alpha '.mat'],'cp' , 'xs');
-    xlim([xmin xmax]); axis 'auto y'
-    plot(xs,-cp,'color',colors(i,:),'DisplayName',geometry,'LineWidth', 1.2)
-    legend
-    hold off
-    subplot(3,1,3)
-    title('Pressure gradients') 
-    hold on
-    load(['Data/' geometry '/' Re '_' alpha '.mat'],'cp' , 'xs');
-    xlim([xmin xmax]); axis 'auto y';
-    [xgrads, cpgrads] = cp_gradjc(xs,cp,np);
-    plot(xgrads, -cpgrads,'color',colors(i,:),'DisplayName',geometry,'LineWidth', 1.2);
-    legend;
-    hold off
+%     subplot(3,1,2)
+%     title('Pressure distrubution') 
+%     hold on
+%     load(['Data/' geometry '/' Re '_' alpha '.mat'],'cp' , 'xs');
+%     xlim([xmin xmax]); axis 'auto y'
+%     plot(xs,-cp,'color',colors(i,:),'DisplayName',geometry,'LineWidth', 1.2)
+%     legend
+%     hold off
+%     subplot(3,1,3)
+%     title('Pressure gradients') 
+%     hold on
+%     load(['Data/' geometry '/' Re '_' alpha '.mat'],'cp' , 'xs');
+%     xlim([xmin xmax]); axis 'auto y';
+%     [xgrads, cpgrads] = cp_gradjc(xs,cp,np);
+%     plot(xgrads, -cpgrads,'color',colors(i,:),'DisplayName',geometry,'LineWidth', 1.2);
+%     legend;
+%     hold off
 end
 
 set(h, 'Visible', get(0,'DefaultFigureVisible'))
